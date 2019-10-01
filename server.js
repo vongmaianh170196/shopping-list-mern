56fd1e06989cd4df8af212a00d7182ae72da4af8
Anh <anh.vo@student.lamk.fi>
2019-10-01 11:34:28 +0300

    ...

--- server.js
+++ server.js
@@ -34,6 +34,16 @@ if (process.env.NODE_ENV === 'production') {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
   }
+
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+    });
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('Server started on port ${port}'));
