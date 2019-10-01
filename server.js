a63c4214bfe984b4b728b52af5e3c1a15c04e6f1
Anh <anh.vo@student.lamk.fi>
2019-10-01 10:57:14 +0300

    finish redux

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
+    
 const items = require('./Routes/api/items');
 
 
@@ -22,6 +23,17 @@ mongoose
 //Use Routes
 app.use('/api/items', items)
 
+//Serve static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('Server started on port ${port}'));
